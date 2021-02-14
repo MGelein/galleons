9c7c5b048853f345852ce1aac0e4e97c14b1dee5
Mees Gelein <meesgelein@gmail.com>
2021-02-15 00:53:03 +0100

    add shadows to make countdown more legible

--- src/countdown.lua
+++ src/countdown.lua
@@ -24,8 +24,10 @@ end
 function countdown.draw()
     love.graphics.draw(countdown.sprite, countdown.x, countdown.y, 0, countdown.s, countdown.s, countdown.ox, countdown.oy)
     love.graphics.setFont(fonts.countdown)
+    love.graphics.setColor(fonts.black.r, fonts.black.g, fonts.black.b, 1)
     love.graphics.print(countdown.timeString, countdown.x - countdown.ox + 20, countdown.y - countdown.oy + 10)
     love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.print(countdown.timeString, countdown.x - countdown.ox + 19, countdown.y - countdown.oy + 9)
 end
 
 function countdown.update()
