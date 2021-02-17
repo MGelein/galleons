e85a7ea13156bca67b4f7ddec994c476289426f9
Mees Gelein <meesgelein@gmail.com>
2021-02-17 22:09:11 +0100

    Creating main menu finally

---
+++ src/gamestates/mainmenu.lua
@@ -0,0 +1,19 @@
+mainmenu = {}
+
+function mainmenu.load()
+    background.create()
+end
+
+function mainmenu.start()
+end
+
+function mainmenu.stop()
+end
+
+function mainmenu.draw()
+    background.draw()
+end
+
+function mainmenu.update(dt)
+    background.update()
+end
