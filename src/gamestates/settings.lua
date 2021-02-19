822a6d0f9ee5a0d7cea81bc336f8da01e3093b99
Mees Gelein <meesgelein@gmail.com>
2021-02-19 17:02:00 +0100

    Maybe hold on to this for a while, and first make the mainmenu work better

--- src/gamestates/settings.lua
+++ src/gamestates/settings.lua
@@ -10,8 +10,37 @@ end
 
 function settings.draw()
     background.draw()
+    settings.drawButtonPrompts()
 end
 
 function settings.update(dt)
     background.update()
+
+    if controller.A ~= nil then
+        settings.parseControlInput(controller.A)
+    end
+end
+
+function settings.parseControlInput(controller)
+    
+end
+
+function settings.drawButtonPrompts()
+    love.graphics.setFont(fonts.place)
+    love.graphics.push()
+    love.graphics.translate(520, 0)
+    love.graphics.circle('fill', 25, 45, 10)
+    love.graphics.setColor(0.2, 0.8, 0.2)
+    love.graphics.draw(sprites.ui_A, 0, 20)
+    love.graphics.setColor(1, 1, 1, 1)
+    fonts.outlineText('Apply', 50, 15, 800, 'left')
+    
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.circle('fill', 325, 45, 10)
+    love.graphics.setColor(0.8, 0.8, 0.2)
+    love.graphics.draw(sprites.ui_Y, 300, 20)
+    love.graphics.setColor(1, 1, 1, 1)
+    
+    fonts.outlineText('Main Menu', 350, 15, 800, 'left')
+    love.graphics.pop()
 end
